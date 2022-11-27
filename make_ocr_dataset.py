4f4bdbff58a77575d69db39b2aa0c0dcb87b428b
Neha Nishikant <nnishika@tir.lti.cs.cmu.edu>
2022-11-26 22:25:52 -0500

    made ocr dataset

--- make_ocr_dataset.py
+++ make_ocr_dataset.py
@@ -2,24 +2,32 @@ import pytesseract
 from pytesseract import Output
 from PIL import Image
 import json
+import argparse
 
 
 def make_dataset(split):
 
+    path = "/projects/tir3/users/nnishika/MML/data-of-multimodal-sarcasm-detection/"
+
+    textpath = path+"text/"
     if split == "train":
-        f_in = ?
-    elif split == "test":
-        f_in = ?
+        f_in = open(textpath+"train.txt", "r")
     else:
-        f_in = ?
+        f_in = open(textpath+split+"2.txt", "r")
     
     data = []
+    for line in f_in.readlines():
 
-    for _ in _:
+        img_id = eval(line)[0]
 
-        img = Image.open('testimages/726506255683039232.jpg')
+        imgpath = path+"dataset_image/"+img_id+'.jpg'
+        try:
+            img = Image.open(imgpath)
+        except:
+            print("bad img id: ", img_id)
+            continue
+
         d = pytesseract.image_to_data(img, output_type=Output.DICT)
-
         n_boxes = len(d['level'])
         ocr_tokens = []
         ocr_info = []
@@ -30,7 +38,7 @@ def make_dataset(split):
             if d['conf'][i] < 0 or len(token) == 0:
                 continue
 
-            print(token, d['conf'][i])
+            # print(token, d['conf'][i])
 
             (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
 
@@ -46,17 +54,24 @@ def make_dataset(split):
                 }
             })
 
-            data.append({
-                'image_id': ?,
-                'ocr_tokens': ocr_tokens,
-                'ocr_info': ocr_info
-            })
+        data.append({
+            'image_id': img_id,
+            'ocr_tokens': ocr_tokens,
+            'ocr_info': ocr_info
+        })
 
     d_out = {
         'dataset_name': 'sarcasm',
-        'dataset_type': 'train',
+        'dataset_type': split,
         'dataset_verion': 1,
         'data': data
         }
+    f_out = open("dataset/sarcasm_ocr_"+split+".json", "w")
     json.dump(d_out, f_out, indent=4)
-            
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('split', type=str, default=None)
+    args = parser.parse_args()
+    make_dataset(args.split)
